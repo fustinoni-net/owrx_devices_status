888fd7b0564659c7a02e61c8ebc651ac41f39534
fustinoni-net <enrico.fustinoni@gmail.com>
2026-01-02 15:20:16 +0100

    Fix for mqtt messages for external resources update

--- owrx_devices_staus.py
+++ owrx_devices_staus.py
@@ -15,172 +15,19 @@ class Settings(BaseSettings):
     mqtt_broker_port: int = 1883
     mqtt_username: str | None = None
     mqtt_password: str | None = None
-
-    # MQTT topics
-    # mqtt_topic_device_state: str = "openwebrx/hp-owrx/RX"
-    mqtt_topic_device_state: str = "openwebrx/RX"
-
+    mqtt_topic_device_status: str = "openwebrx/RX"
     # receiver url
-    owrx_receiver_url: str = "http://localhost:8073"
+    owrx_receiver_url: str = "http://localhost:8073/status.json"
 
 
 settings = Settings()
-logging.info(settings.mqtt_broker_url)
-logging.info(settings.mqtt_broker_port)
-logging.info(settings.mqtt_topic_device_state)
+logging.info(f"mqtt broker url: {settings.mqtt_broker_url}")
+logging.info(f"mqtt broker pot: {settings.mqtt_broker_port}")
+logging.info(f"mqtt topic for devices staus: {settings.mqtt_topic_device_status}")
+logging.info(f"OWRX URL of the status.json resource:  {settings.owrx_receiver_url}")
 
 def get_devices_profiles(url: str) -> tuple[list[str], str]:
-
     logging.info(f"get_devices_profiles: Fetching SDR profiles from {url}")
-    # st = {"receiver": {
-    #     "name": "<font color='black'  style='background-color: rgba(255, 255, 255, 0.5);'>  IK2ZSH </font>",
-    #     "admin": "example@example.com", "gps": {"lat": 45.7007212405664, "lon": 9.65270348173823}, "asl": 300,
-    #     "location": "<font color='black'  style='background-color: rgba(255, 255, 255, 0.5);'>  Bergamo, Bg, Italy  </font>"},
-    #       "max_clients": 5, "version": "v1.2.103", "sdrs": [{"name": "RTL-SDR v4", "type": "RtlSdrSource", "profiles": [
-    #         {"name": "439.200 MHz", "center_freq": 439200000, "sample_rate": 2400000},
-    #         {"name": "437.200 MHz", "center_freq": 437200000, "sample_rate": 2400000},
-    #         {"name": "435.200 MHz", "center_freq": 435200000, "sample_rate": 2400000},
-    #         {"name": "433.200 MHz", "center_freq": 433200000, "sample_rate": 2400000},
-    #         {"name": "431.200 MHz - 70cm Repeaters", "center_freq": 431200000, "sample_rate": 2400000},
-    #         {"name": "145 MHz 2m", "center_freq": 145000000, "sample_rate": 2400000},
-    #         {"name": "29.5 MHz", "center_freq": 29500000, "sample_rate": 2400000},
-    #         {"name": "29.5 MHz vert 11m 1/4\u03bb", "center_freq": 29500000, "sample_rate": 2400000},
-    #         {"name": "27.5 MHz", "center_freq": 27500000, "sample_rate": 2400000},
-    #         {"name": "27.5 MHz  vert 11m 1/4\u03bb", "center_freq": 27500000, "sample_rate": 2400000},
-    #         {"name": "25.5 MHz", "center_freq": 25500000, "sample_rate": 2400000},
-    #         {"name": "25.5 MHz  vert 11m 1/4\u03bb", "center_freq": 25500000, "sample_rate": 2400000},
-    #         {"name": "23.5 MHz", "center_freq": 23500000, "sample_rate": 2400000},
-    #         {"name": "21.5 MHz", "center_freq": 21500000, "sample_rate": 2400000},
-    #         {"name": "19.5 MHz", "center_freq": 19500000, "sample_rate": 2400000},
-    #         {"name": "17.5 MHz", "center_freq": 17500000, "sample_rate": 2400000},
-    #         {"name": "15.5 MHz", "center_freq": 15500000, "sample_rate": 2400000},
-    #         {"name": "13.5 MHz", "center_freq": 13500000, "sample_rate": 2400000},
-    #         {"name": "11.5 MHz", "center_freq": 11500000, "sample_rate": 2400000},
-    #         {"name": "9.5 MHz", "center_freq": 9500000, "sample_rate": 2400000},
-    #         {"name": "7.5 MHz", "center_freq": 7500000, "sample_rate": 2400000},
-    #         {"name": "5.5 MHz", "center_freq": 5500000, "sample_rate": 2400000},
-    #         {"name": "3.5 MHz", "center_freq": 3500000, "sample_rate": 2400000},
-    #         {"name": "1.5 MHz", "center_freq": 1500000, "sample_rate": 2400000},
-    #         {"name": "137.0 MHz", "center_freq": 137000000, "sample_rate": 2400000},
-    #         {"name": "135.0 MHz", "center_freq": 135000000, "sample_rate": 2400000},
-    #         {"name": "133.0 MHz", "center_freq": 133000000, "sample_rate": 2400000},
-    #         {"name": "131.0 MHz", "center_freq": 131000000, "sample_rate": 2400000},
-    #         {"name": "129.0 MHz", "center_freq": 129000000, "sample_rate": 2400000},
-    #         {"name": "127.0 MHz", "center_freq": 127000000, "sample_rate": 2400000},
-    #         {"name": "125.0 MHz", "center_freq": 125000000, "sample_rate": 2400000},
-    #         {"name": "123.0 MHz", "center_freq": 123000000, "sample_rate": 2400000},
-    #         {"name": "121.0 MHz", "center_freq": 121000000, "sample_rate": 2400000},
-    #         {"name": "119.0 MHz", "center_freq": 119000000, "sample_rate": 2400000},
-    #         {"name": "1090MHz ADSB", "center_freq": 1090000000, "sample_rate": 2400000},
-    #         {"name": "ISM 433", "center_freq": 433900000, "sample_rate": 2400000},
-    #         {"name": "PMR", "center_freq": 446000000, "sample_rate": 2400000},
-    #         {"name": "PMR 433,600", "center_freq": 433600000, "sample_rate": 2600000},
-    #         {"name": "40.680 MHz - 8m", "center_freq": 40680000, "sample_rate": 1800000}]},
-    #                                                         {"name": "SDRPlay", "type": "SdrplaySource", "profiles": [
-    #                                                             {"name": "10m", "center_freq": 28850000,
-    #                                                              "sample_rate": 2000000},
-    #                                                             {"name": "11m CB", "center_freq": 27000000,
-    #                                                              "sample_rate": 2000000},
-    #                                                             {"name": "12m", "center_freq": 24940000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "15m", "center_freq": 21225000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "17m", "center_freq": 18118000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "20m", "center_freq": 14175000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "30m", "center_freq": 10125000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "40m", "center_freq": 7150000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "60m", "center_freq": 5350000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "80m", "center_freq": 3750000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "160m", "center_freq": 1900000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "AM Broadcast", "center_freq": 1100000,
-    #                                                              "sample_rate": 2000000},
-    #                                                             {"name": "49m Broadcast", "center_freq": 6050000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "41m Broadcast", "center_freq": 7325000,
-    #                                                              "sample_rate": 250000},
-    #                                                             {"name": "31m Broadcast", "center_freq": 9650000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "25m Broadcast", "center_freq": 11850000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "22m Broadcast", "center_freq": 13720000,
-    #                                                              "sample_rate": 500000},
-    #                                                             {"name": "433MHz ISM", "center_freq": 433000000,
-    #                                                              "sample_rate": 2000000},
-    #                                                             {"name": "1090MHz ADSB", "center_freq": 1090000000,
-    #                                                              "sample_rate": 4000000},
-    #                                                             {"name": "2m", "center_freq": 145000000,
-    #                                                              "sample_rate": 5000000},
-    #                                                             {"name": "30-40m", "center_freq": 8500000,
-    #                                                              "sample_rate": 4000000}]},
-    #                                                         {"name": "Nooelec sdr", "type": "RtlSdrSource",
-    #                                                          "profiles": [
-    #                                                              {"name": "437.200 MHz", "center_freq": 437200000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "435.200 MHz", "center_freq": 435200000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "433.200 MHz", "center_freq": 433200000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "431.200 MHz - 70cm Repeaters",
-    #                                                               "center_freq": 431200000, "sample_rate": 2400000},
-    #                                                              {"name": "145 MHz 2m", "center_freq": 145000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "53.58 MHz", "center_freq": 52800000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "51.0 MHz", "center_freq": 51000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "29.5 MHz", "center_freq": 29500000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "27.5 MHz", "center_freq": 27500000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "137.0 MHz", "center_freq": 137000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "135.0 MHz", "center_freq": 135000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "133.0 MHz", "center_freq": 133000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "131.0 MHz", "center_freq": 131000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "129.0 MHz", "center_freq": 129000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "127.0 MHz", "center_freq": 127000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "125.0 MHz", "center_freq": 125000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "123.0 MHz", "center_freq": 123000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "121.0 MHz", "center_freq": 121000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "119.0 MHz", "center_freq": 119000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "1090MHz ADSB", "center_freq": 1090000000,
-    #                                                               "sample_rate": 2400000},
-    #                                                              {"name": "DAB 6A", "center_freq": 181936000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 7A", "center_freq": 188928000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 7D", "center_freq": 194064000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 9A", "center_freq": 202928000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 9C", "center_freq": 206352000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 9D", "center_freq": 208064000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 11B", "center_freq": 218640000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 11C", "center_freq": 220352000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 12B", "center_freq": 225648000,
-    #                                                               "sample_rate": 2048000},
-    #                                                              {"name": "DAB 12C", "center_freq": 227360000,
-    #                                                               "sample_rate": 2048000}]}]}
 
     try:
         resp = requests.get(url, timeout=10)
@@ -190,7 +37,7 @@ def get_devices_profiles(url: str) -> tuple[list[str], str]:
         logging.error("get_devices_profiles: error getting profile from %s: %s", url, exc)
         st ={}
 
-
+    logging.info(f"get_devices_profiles: Json used to get profiles: {st}")
     sdr_names: list[str] = list(map(lambda sdr: sdr["name"], filter(lambda sdr: "name" in sdr, st["sdrs"])))
     start_profile_name: str = st["sdrs"][0]["profiles"][0]["name"]
 
@@ -213,13 +60,10 @@ class OwrxDevicesStatus:
         self.mqtt_client = mqtt.Client(client_id="OwrxDevicesStatus", protocol=mqtt.MQTTv311)
         self.mqtt_client.on_connect = self._on_connect
         self.mqtt_client.on_message = self._on_message
-        self.topic_device_state = settings.mqtt_topic_device_state
+        self.topic_device_state = settings.mqtt_topic_device_status
 
-        self.devices_status_change_listeners: list[Callable[[dict[str, str], dict[str, str]], None]] = []
-        self._notify_devices_status_change(self.devices, self.device_profile)
-
-    def get_devices_status(self) -> tuple[dict[str, str], dict[str, str]]:
-        return self.devices, self.device_profile
+        self.devices_status_change_listeners: list[Callable[[dict[str, str]], None]] = []
+        self._notify_devices_status_change(self.prepare_massage_to_send())
 
 
     def set_devices_from_receiver(self):
@@ -270,26 +114,34 @@ class OwrxDevicesStatus:
             if payload.state == "ServerStarted":
                 self.set_devices_from_receiver()
             else:
-                self.devices.update({payload.source: payload.state})
+                if payload.source in self.devices.keys():
+                    self.devices.update({payload.source: payload.state})
         else:
             logging.info(f"OwrxDevicesStatus: payload profile: {payload.profile}")
             if payload.profile:
-                self.device_profile.update({payload.source: payload.profile})
+                if payload.source in self.devices.keys():
+                    self.device_profile.update({payload.source: payload.profile})
 
 
         logging.info(self.devices)
         logging.info(self.device_profile)
         # Broadcast the message to all connected clients
-        self._notify_devices_status_change(self.devices, self.device_profile)
+        self._notify_devices_status_change(self.prepare_massage_to_send())
 
 
-    def set_devices_status_change_listener(self, devices_status_has_changed: Callable[[dict[str, str], dict[str, str]], None]) -> None:
+
+    def prepare_massage_to_send(self) -> dict[str, str]:
+        latest_message: dict[str, str] = {name: status if status == "Stopped" else self.device_profile[name] if name in self.device_profile.keys() else "Running" for name, status in self.devices.items()}
+        return latest_message
+
+
+    def set_devices_status_change_listener(self, devices_status_has_changed: Callable[[dict[str, str]], None]) -> None:
         self.devices_status_change_listeners.append(devices_status_has_changed)
 
 
-    def _notify_devices_status_change(self, devices: dict[str, str], device_profile: dict[str, str]):
+    def _notify_devices_status_change(self, message: dict[str, str]):
         for listener in self.devices_status_change_listeners:
-            listener(devices, device_profile)
+            listener(message)
 
 
 
